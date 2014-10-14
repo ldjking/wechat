92922b2e97d6f6828a874257754c529f1b9c07b7
ldjking <ldjbox@126.com>
2014-10-14 19:13:16 +0800

    调整weixin 接口路径

---
+++ test/handler/test_weixin.js
@@ -0,0 +1,10 @@
+var request=require("request");
+var url="http://localhost/handler/weixin?signature=aaa&timestamp=313123123&nonce=132312&echostr=fster";
+request(url, function (error, response, body) {
+	if(error){
+		console.log("error");
+	}
+	else{
+		console.log("body:",body);
+	}
+});
